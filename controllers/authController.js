86f2aa2b37cab6374215812ece7f14d4f15349d2
Queen <samuellqueenie@gmail.com>
2025-07-29 15:37:50 +0100

    Token expiration extended

--- controllers/authController.js
+++ controllers/authController.js
@@ -347,7 +347,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: user._id, role: user.role, phoneNumber: user.phoneNumber }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user._id, role: user.role, phoneNumber: user.phoneNumber }, process.env.JWT_SECRET, { expiresIn: "20h" });
 
     res.json({ message: "Login successful", role, token, id: user._id, phoneNumber, firstname: user.firstName, lastname: user.lastName });
 
